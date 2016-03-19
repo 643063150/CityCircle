77b38f483fb1b62d113bd8352cc76e9e4c9fe88e
飞侠 <643063150>
2016-03-19 23:09:17 +0800

    house

--- app/src/main/java/citycircle/com/Property/SelectHome.java
+++ app/src/main/java/citycircle/com/Property/SelectHome.java
@@ -1,6 +1,8 @@
 package citycircle.com.Property;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,11 +13,18 @@ import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.bigkoo.alertview.AlertView;
 import com.bigkoo.alertview.OnItemClickListener;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import citycircle.com.R;
 import citycircle.com.Utils.GlobalVariables;
+import citycircle.com.Utils.HttpRequest;
 
 /**
  * Created by admins on 2016/1/30.
@@ -25,12 +34,18 @@ public class SelectHome extends Fragment implements OnItemClickListener, View.On
     Animation translateAnimation;
     int types;
     Button calm, village, building, unit, floor, room, addhouse;
-    String url,urlstr;
+    String url, urlstr, calmid, villageid, buildingid, unitid, floorid, roomid, addurl;
+    ArrayList<HashMap<String, String>> array = new ArrayList<HashMap<String, String>>();
+    HashMap<String, String> hashMap;
+    String alretstr[];
+    int type = 0;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.selecthome, container, false);
         intview();
         types = getActivity().getIntent().getIntExtra("types", 0);
+//        Alertshow(new String[]{"其他按钮1", "其他按钮2", "其他按钮3"});
         return view;
     }
 
@@ -54,6 +69,59 @@ public class SelectHome extends Fragment implements OnItemClickListener, View.On
         addhouse.setOnClickListener(this);
     }
 
+    private void getStr(final int type) {
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                HttpRequest httpRequest = new HttpRequest();
+                urlstr = httpRequest.doGet(url);
+                if (urlstr.equals("网络超时")) {
+                    handler.sendEmptyMessage(2);
+                } else {
+                    handler.sendEmptyMessage(1);
+                }
+            }
+        }.start();
+    }
+
+    public void setArray(String str) {
+        JSONObject jsonObject = JSON.parseObject(str);
+        JSONObject jsonObject1 = jsonObject.getJSONObject("data");
+        int a = jsonObject1.getIntValue("code");
+        if (a == 0) {
+            JSONArray jsonArray = jsonObject1.getJSONArray("info");
+            alretstr = new String[jsonArray.size()];
+            for (int i = 0; i < jsonArray.size(); i++) {
+                hashMap = new HashMap<>();
+                JSONObject jsonObject2 = jsonArray.getJSONObject(i);
+                hashMap.put("id", jsonObject2.getString("id") == null ? "" : jsonObject2.getString("id"));
+                hashMap.put("title", jsonObject2.getString("title") == null ? "" : jsonObject2.getString("title"));
+                alretstr[i] = jsonObject2.getString("title");
+                array.add(hashMap);
+            }
+        }
+    }
+
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case 1:
+                    setArray(urlstr);
+                    Alertshow(alretstr);
+                    break;
+                case 2:
+                    Toast.makeText(getActivity(), R.string.intent_error, Toast.LENGTH_SHORT).show();
+                    break;
+                case 3:
+                    Toast.makeText(getActivity(), R.string.nomore, Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    };
+
     private void Alertshow(String str[]) {
         new AlertView(null, null, null, null,
                 str, getActivity(), AlertView.Style.Alert, this).show();
@@ -62,7 +130,26 @@ public class SelectHome extends Fragment implements OnItemClickListener, View.On
     //弹出框点击
     @Override
     public void onItemClick(Object o, int i) {
-        Toast.makeText(getActivity(), i + "", Toast.LENGTH_SHORT).show();
+        if (type == 0) {
+            calmid = array.get(i).get("id");
+            calm.setText(array.get(i).get("title"));
+        } else if (type == 1) {
+            villageid = array.get(i).get("id");
+            village.setText(array.get(i).get("title"));
+        } else if (type == 2) {
+            buildingid = array.get(i).get("id");
+            building.setText(array.get(i).get("title"));
+        } else if (type == 3) {
+            unitid = array.get(i).get("id");
+            unit.setText(array.get(i).get("title"));
+        } else if (type == 4) {
+            floorid = array.get(i).get("id");
+            floor.setText(array.get(i).get("title"));
+        }else if (type==5){
+            roomid=array.get(i).get("id");
+            room.setText(array.get(i).get("title"));
+            addhouse.setBackgroundResource(R.drawable.btn_bg);
+        }
     }
 
     //按钮点击
@@ -70,19 +157,60 @@ public class SelectHome extends Fragment implements OnItemClickListener, View.On
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.calm:
-//                Alertshow(new String[]{"其他按钮1", "其他按钮2", "其他按钮3", "其他按钮4", "其他按钮5", "其他按钮6",
-//                "其他按钮7", "其他按钮8", "其他按钮9", "其他按钮10", "其他按钮11", "其他按钮12"});
-                url= GlobalVariables.urlstr+"Common.getHouseList&pid=0";
+                array.clear();
+                type = 0;
+                url = GlobalVariables.urlstr + "Common.getHouseList&pid=0";
+                getStr(0);
                 break;
             case R.id.village:
+                type = 1;
+                if (calmid == null) {
+                    calm.startAnimation(translateAnimation);
+                } else {
+                    array.clear();
+                    url = GlobalVariables.urlstr + "Common.getHouseList&pid=" + calmid;
+                    getStr(0);
+                }
                 break;
             case R.id.building:
+                type = 2;
+                if (villageid == null) {
+                    village.startAnimation(translateAnimation);
+                } else {
+                    array.clear();
+                    url = GlobalVariables.urlstr + "Common.getHouseList&pid=" + villageid;
+                    getStr(0);
+                }
                 break;
             case R.id.unit:
+                type = 3;
+                if (buildingid == null) {
+                    building.startAnimation(translateAnimation);
+                } else {
+                    array.clear();
+                    url = GlobalVariables.urlstr + "Common.getHouseList&pid=" + buildingid;
+                    getStr(0);
+                }
                 break;
             case R.id.floor:
+                type = 4;
+                if (unitid == null) {
+                    unit.startAnimation(translateAnimation);
+                } else {
+                    array.clear();
+                    url = GlobalVariables.urlstr + "Common.getHouseList&pid=" + unitid;
+                    getStr(0);
+                }
                 break;
             case R.id.room:
+                type = 5;
+                if (floorid == null) {
+                    floor.startAnimation(translateAnimation);
+                } else {
+                    array.clear();
+                    url = GlobalVariables.urlstr + "Common.getHouseList&pid=" + floorid;
+                    getStr(0);
+                }
                 break;
             case R.id.addhouse:
                 break;
